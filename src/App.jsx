beb66d9256a3f72cfa41e822fe2a5709a11c53a2
CristinaColomoiets <colomoetscristina@gmail.com>
2024-05-05 14:02:43 +0200

    lab finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import {Route, Routes} from 'react-router-dom';
+import studentsData from "./assets/students.json"; // Traigo los datos de Json para poder obtener el id de cada alumno
 
 
-function App() {
+
+function App({_id}) {
 
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
 
       <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
+        <Routes>
+          <Route path={'/'} element={<HomePage />}></Route>
+          {/* dos puntos en la ruta indica que alli va un param */}
+          <Route path={'/students/:studentId'} element={<StudentDetailsPage/>}></Route> 
+          <Route path={'/profile'} element={<UserProfilePage />}></Route>
+        </Routes>
         <UserProfilePage />
       </div>
 
